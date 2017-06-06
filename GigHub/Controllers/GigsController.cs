78a643b4c69e43feba7b0b375c2100654e38e983
shiva sharma <shivathebravo@hotmail.com>
2017-06-05 23:38:37 -0400

    Added upcoming gigs

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,6 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -15,7 +16,23 @@ namespace GigHub.Controllers
         {
             _context = new ApplicationDbContext();
         }
-        [System.Web.Mvc.Authorize]
+
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)
+                .ToList();
+            return View(gigs);
+        }
+
+
+
+
+        [Authorize]
         public ActionResult Attending()
         {
             var userId = User.Identity.GetUserId();
@@ -37,7 +54,7 @@ namespace GigHub.Controllers
         }
 
 
-        [System.Web.Mvc.Authorize]
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -68,7 +85,7 @@ namespace GigHub.Controllers
             };
             _context.Gigs.Add(gig);
             _context.SaveChanges();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
